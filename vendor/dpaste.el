b3cbe41831baf3dd94faeae3dd4450f22ae4a83a
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2009-05-10 13:10:17 +0200

    Modified dpaste to work.

--- vendor/dpaste.el
+++ vendor/dpaste.el
@@ -1,7 +1,7 @@
 ;;; dpaste.el --- Emacs integration for dpaste.com
 
 ;; Copyright (C) 2008 Greg Newman <20seven.org>
-     
+
 ;; Version: 0.1.1
 ;; Keywords: paste pastie pastebin dpaste python
 ;; Created: 01 Dec 2008
@@ -24,11 +24,11 @@
 ;; along with GNU Emacs; see the file COPYING.  If not, write to the
 ;; Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
 ;; MA 02111-1307, USA.
- 
+
 
 ;;; Commentary:
 ;;  dpaste.el will post a region or if no region is selected, the buffer to http://dpaste.com and
-;;  and put the url into the kill-ring 
+;;  and put the url into the kill-ring
 ;;  current api usage example: curl -si -F 'content=<-' http://dpaste.com/api/v1/ | grep ^Location: | colrm 1 10
 
 ;;  Thanks to Paul Bissex (http://news.e-scribe.com) for a great paste service
@@ -60,17 +60,14 @@
          (name (file-name-nondirectory file))
          (ext (or (cdr (assoc major-mode dpaste-supported-modes-alist))
                   (file-name-extension file)
-                  "txt"))
-         (output (generate-new-buffer " *dpaste*")))
-    (shell-command-on-region
-     begin end
-     (format (concat "curl -si "
-                     "-F 'language=%s' "
-                     "-F 'content=<-' "
-                     "http://dpaste.com/api/v1/")ext name)
-     output)
+                  ""))
+         (output (generate-new-buffer "*dpaste*")))
+    (shell-command-on-region begin end
+			     (concat "curl -si -F 'language=" ext
+				     "' -F 'content=<-' http://dpaste.com/api/v1/")
+			     output)
     (with-current-buffer output
-      (search-backward-regexp "^Location: \\([A-Za-z0-9]+.*\\)")
+      (search-backward-regexp "^Location: \\([A-Za-z0-9]+.*\\)$")
       (message "dpaste created: %s" (match-string 1))
       (kill-new (match-string 1)))
    (kill-buffer output)))
