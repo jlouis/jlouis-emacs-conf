9eaa034ce8e52741e5632b20eccf92ac8540d8aa
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2009-05-16 12:15:14 +0200

    Install magit mode.

--- configs/magit-setup.el
+++ configs/magit-setup.el
@@ -1,5 +1,4 @@
-;;; init.el --- Initialization file for my Emacs setup
-
+;;; Twelf!
 ; Copyright (c) 2009, Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
 ; All rights reserved.
 
@@ -29,57 +28,5 @@
 ; ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 ; POSSIBILITY OF SUCH DAMAGE.
 
-;;; Commentary:
+(require 'magit)
 
-;; Get up and running, set custom to something else, and set up load paths
-;; for the rest of the system. Then proceed to load each configuration file
-;; for each module installed in emacs via load-cfg-files.
-;;
-;; TODO:
-;;  ffap support
-;;
-;;; Code:
-(setq custom-file "~/.emacs.d/custom.el")
-(load custom-file)
-
-(defconst *emacs-config-dir* "~/.emacs.d/configs/" "")
-(defun load-cfg-files (files)
-  (dolist (f files)
-    (load (expand-file-name
-	   (concat *emacs-config-dir* f)))
-    (message "Loaded config file: %s" file)))
-
-(defun extend-load-path (path)
-  (setq load-path (cons (expand-file-name path) load-path)))
-
-(extend-load-path "~/.emacs.d/vendor")
-(extend-load-path "~/.emacs.d")
-;; Get vendor stuff loaded as well
-(progn (cd "~/.emacs.d/vendor")
-       (normal-top-level-add-subdirs-to-load-path))
-
-(load-cfg-files '("global"
-		  "c-mode-setup"
-		  "color-theme-setup"
-		  "diredx-setup"
-		  "epa-setup"
-		  "hippie-expand-setup"
-		  "ido-setup"
-		  "js2-setup"
-		  "nxml-setup"
-		  "org-setup"
-		  "proof-general-setup"
-		  "sml-setup"
-		  "tex-code"
-		  "tramp-setup"
-		  "tuareg-setup"
-		  "twelf-setup"
-		  "dpaste-setup"
-		  "gist-setup"
-		  "magit-setup"
-		  "browse-kill-ring-setup"))
-
-;; Get back to homedir
-(cd "~")
-
-;;; init.el ends here
