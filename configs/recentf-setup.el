f1e258f3b5f70f2d2ca0e6f19ff0d2b7b5f6b983
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2011-02-09 20:38:27 +0100

    Push in recentf-setup

---
+++ configs/recentf-setup.el
@@ -0,0 +1,11 @@
+(require 'recentf)
+(recentf-mode 1)
+
+(defun recentf-ido-find-file ()
+  "Find a recent file using Ido."
+  (interactive)
+  (let ((file (ido-completing-read "Choose recent file: " recentf-list nil t)))
+    (when file
+      (find-file file))))
+
+(global-set-key "\C-x\C-r" 'recentf-ido-find-file)
--- init.el
+++ init.el
@@ -1,34 +1,4 @@
 ;; init.el --- Initialization file for my Emacs setup
-
-; Copyright (c) 2009, Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
-; All rights reserved.
-
-; Redistribution and use in source and binary forms, with or
-; without modification, are permitted provided that the following
-; conditions are met:
-
-; * Redistributions of source code must retain the above
-;   copyright notice, this list of conditions and the following
-;   disclaimer.
-
-; * Redistributions in binary form must reproduce the above
-;   copyright notice, this list of conditions and the following
-;   disclaimer in the documentation and/or other materials
-;   provided with the distribution.
-
-; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-; "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-; LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-; FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-; COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-; INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-; BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-; CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-; LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-; ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-; POSSIBILITY OF SUCH DAMAGE.
-
 ;;; Commentary:
 
 ;; Get up and running, set custom to something else, and set up load paths
@@ -77,7 +47,6 @@
 (require 'ffap)
 (require 'uniquify)
 (require 'ansi-color)
-(require 'recentf)
 
 (load-config-files  '("defuns" ;; Has to go first
 		      "global" ;; Has to go second
@@ -96,6 +65,7 @@
 		      "nxml-setup"
 		      "org-setup"
 		      "proof-general-setup"
+		      "recentf-setup"
 		      "tex-code"
 		      "tramp-setup"
 		      "tuareg-setup"
