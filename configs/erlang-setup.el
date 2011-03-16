37dd21087e07a07f2d13570052f098ef5fa3dc6a
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2011-03-16 21:34:19 +0100

    New Erlang version, new tools version.

--- configs/erlang-setup.el
+++ configs/erlang-setup.el
@@ -1,7 +1,7 @@
 
 
 (setq erlang-root-dir "/usr/local/lib/erlang")
-(setq tools-ver "2.6.6.2")
+(setq tools-ver "2.6.6.3")
 (setq load-path (cons (concat erlang-root-dir "/lib/tools-" tools-ver "/emacs")
 		      load-path))
 (setq exec-path (cons (concat erlang-root-dir "/bin")
