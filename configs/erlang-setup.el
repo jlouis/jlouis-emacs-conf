df3c1b304490df4b152667f43455771593c7777f
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2011-08-23 16:07:36 +0200

    Add Wrangler to the Erlang setup.

--- configs/erlang-setup.el
+++ configs/erlang-setup.el
@@ -1,9 +1,17 @@
 
 
 (setq erlang-root-dir "/usr/local/lib/erlang")
-(setq tools-ver "2.6.6.3")
+(setq tools-ver "2.6.6.4")
 (setq load-path (cons (concat erlang-root-dir "/lib/tools-" tools-ver "/emacs")
 		      load-path))
 (setq exec-path (cons (concat erlang-root-dir "/bin")
 		      exec-path))
 (require 'erlang-start)
+
+;; Wrangler
+
+(add-to-list 'load-path "/usr/local/share/wrangler/elisp")
+(require 'wrangler)
+
+(load-file "/usr/local/share/wrangler/elisp/graphviz-dot-mode.el")
+
