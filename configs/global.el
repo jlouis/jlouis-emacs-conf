577337685d45c1cd098938611ecedcb38ed5048e
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2011-08-23 16:30:08 +0200

    Add completion to the tab mode.

--- configs/global.el
+++ configs/global.el
@@ -55,6 +55,7 @@
 (setq-default shift-select-mode nil)
 (setq-default default-tab-width 8)
 (setq-default indent-tabs-mode nil)
+(setq tab-always-indent 'complete)
 (setq-default line-number-mode t)
 (setq-default column-number-mode t)
 (setq-default mouse-yank-at-point t)
