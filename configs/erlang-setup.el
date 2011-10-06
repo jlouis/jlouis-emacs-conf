8cd755fa2b965469cb5128f5ebe4df271088cd4a
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2011-10-06 23:54:34 +0200

    Bump Erlang tools to a newer version.

--- configs/erlang-setup.el
+++ configs/erlang-setup.el
@@ -1,7 +1,7 @@
 
 
 (setq erlang-root-dir "/usr/local/lib/erlang")
-(setq tools-ver "2.6.6.4")
+(setq tools-ver "2.6.6.5")
 (setq load-path (cons (concat erlang-root-dir "/lib/tools-" tools-ver "/emacs")
 		      load-path))
 (setq exec-path (cons (concat erlang-root-dir "/bin")
