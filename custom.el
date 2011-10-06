412cb3c0b1a4a00d3b493252f3e20f8ef586603f
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2011-10-06 23:55:42 +0200

    Add custom.el to the repository.

---
+++ custom.el
@@ -0,0 +1,62 @@
+;;; CUSTOM SET VARIABLES
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(agda2-highlight-face-groups (quote conor))
+ '(agda2-include-dirs (quote ("." "/home/jlouis/lib/agda/src")))
+ '(auto-compression-mode t nil (jka-compr))
+ '(auto-image-file-mode t)
+ '(blink-cursor-mode nil)
+ '(browse-url-browser-function (quote browse-url-firefox))
+ '(case-fold-search t)
+ '(compilation-ask-about-save nil)
+ '(compilation-read-command nil)
+ '(compilation-scroll-output t)
+ '(current-language-environment "English")
+ '(desktop-enable t)
+ '(dired-dwim-target t)
+ '(display-time-mode t)
+ '(find-file-suppress-same-file-warnings t)
+ '(global-auto-revert-mode t)
+ '(global-font-lock-mode t nil (font-lock))
+ '(global-visual-line-mode nil)
+ '(global-whitespace-mode nil)
+ '(global-whitespace-newline-mode t)
+ '(haskell-indent-offset 4)
+ '(haskell-program-name "ghci")
+ '(indicate-empty-lines t)
+ '(js2-basic-offset 4)
+ '(midnight-mode t nil (midnight))
+ '(quack-default-program "scheme48")
+ '(quack-fontify-style (quote emacs))
+ '(recentf-max-saved-items 500)
+ '(safe-local-variable-values (quote ((erlang-indent-level . 4) (Syntax . Common-lisp))))
+ '(scheme-program-name "scheme48")
+ '(shadow-noquery t)
+ '(show-paren-mode t nil (paren))
+ '(size-indication-mode t)
+ '(slime-startup-animation nil)
+ '(sml-compile-command "CM.make \"sources.cm\"")
+ '(sml-indent-args 2)
+ '(sml-indent-level 2)
+ '(speedbar-frame-parameters (quote ((minibuffer) (width . 20) (border-width . 0) (menu-bar-lines . 0) (tool-bar-lines . 0) (unsplittable . t) (set-background-color "black"))))
+ '(speedbar-show-unknown-files t)
+ '(user-mail-address "jesper.louis.andersen@gmail.com")
+ '(visible-bell t)
+ '(wrangler-search-paths (quote ("/home/jlouis/Projects/etorrent/apps/utp/src" "/home/jlouis/Projects/etorrent/apps/utp/include"))))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
+
+
+
+
+
+
+
+
