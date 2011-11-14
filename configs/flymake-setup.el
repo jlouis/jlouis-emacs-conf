e99b322b6899254674d09db3e737cf2a2737124c
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2011-11-14 13:14:43 +0100

    Enable flymake.

---
+++ configs/flymake-setup.el
@@ -0,0 +1,11 @@
+(require 'flymake)
+(defun flymake-erlang-init ()
+  (let* ((temp-file (flymake-init-create-temp-buffer-copy
+		     'flymake-create-temp-inplace))
+	 (local-file (file-relative-name temp-file
+		(file-name-directory buffer-file-name))))
+    (list (concat
+            emacs-config-dir "/scripts/" "flymake_erlang_script") (list local-file))))
+
+(add-to-list 'flymake-allowed-file-name-masks '("\\.erl\\'" flymake-erlang-init))
+
