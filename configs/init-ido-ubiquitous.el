d4bf4ad2962af535bea5f66744944ecb5e0efe96
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2012-01-20 16:36:10 +0100

    Agda2 mode and also ido-ubiquitous.

---
+++ configs/init-ido-ubiquitous.el
@@ -0,0 +1,7 @@
+(require 'ido-ubiquitous)
+
+(ido-ubiquitous-disable-in find-tag)
+(ido-ubiquitous-disable-in erlang-find-tag)
+
+(provide 'init-ido-ubiquitous)
+;;; init-ido-ubiquitous.el ends here
