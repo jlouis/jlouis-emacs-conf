63db867fe9afba9d0ea1d5b5c61d12486204e098
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2017-12-20 13:56:27 +0100

    Do not make golint into dogma

--- pkg-configs/init-go-mode.el
+++ pkg-configs/init-go-mode.el
@@ -1,7 +1,14 @@
 ;;Custom Compile Command
 
+; go get -u github.com/x/tools/cmd/...
+; go get -u github.com/rogpeppe/godef/...
+; go get -u github.com/nsf/gocode
+; go get -u golang.org/x/tools/cmd/goimports
+; go get -u golang.org/x/tools/cmd/guru
+; go get -u github.com/dougm/goflymake
+
 (defun go-mode-setup ()
-  (setq-local compile-command "go build -v && go test -v && go vet && golint")
+  (setq-local compile-command "go build -v && go test -v && go vet")
   (define-key (current-local-map) "\C-c\C-c" 'compile)
   (set
    (make-local-variable 'company-backends) '(company-go))
