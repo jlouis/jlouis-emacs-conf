078513c3a69a88e717b91222b3623d2bf5fdb234
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2018-11-01 23:13:18 +0100

    Kill a TODO we don't need

--- configs/defuns.el
+++ configs/defuns.el
@@ -43,7 +43,6 @@
          (url (read-from-minibuffer "URL: " default)))
     (switch-to-buffer (url-retrieve-synchronously url))
     (rename-buffer url t)
-    ;; TODO: switch to nxml/nxhtml mode
     (cond ((search-forward "<?xml" nil t) (xml-mode))
           ((search-forward "<html" nil t) (html-mode)))))
 
