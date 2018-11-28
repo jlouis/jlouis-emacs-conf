79f09b53124548de7299a9fcc458abdd9be9e22d
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2018-11-28 22:56:53 +0100

    Try using multiple-cursors a bit and see
    Major change: selected changes the way the region works, by making it
    not delete stuff if you start typing, but you have to type C-d to
    delete the region. I think this is a good trade-off if you are getting
    a selected-keymap in return and can do stuff on selected text.

--- init.el
+++ init.el
@@ -5,10 +5,6 @@
 ;;;
 ;;; Things to check out:
 ;;;
-;;; - phi-search
-;;; - multiple-cursors
-;;; - mc-extras
-;;; - phi-search-mc
 ;;; - nix-mode nix-sandbox nix-buffer
 ;;; - edts (Erlang)
 ;;; - winner
@@ -101,6 +97,18 @@
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
+
+;; ----------------------------------------------------------------------
+;; Keymaps
+
+(define-key input-decode-map [?\C-m] [C-m])
+
+(eval-and-compile
+  (mapc #'(lambda (entry)
+            (define-prefix-command (cdr entry))
+            (bind-key (car entry) (cdr entry)))
+        '(("<C-m>" . my-ctrl-m-map)
+          )))
 
 ;; ----------------------------------------------------------------------
 ;; USE-PACKAGE
@@ -306,6 +314,27 @@
   :config
   (magit-todos-mode))
 
+(use-package mc-extras
+  :ensure t
+  :after multiple-cursors
+  :bind (("<C-m> M-C-f" . mc/mark-next-sexps)
+         ("<C-m> M-C-b" . mc/mark-previous-sexps)
+         ("<C-m> <"     . mc/mark-all-above)
+         ("<C-m> >"     . mc/mark-all-below)
+         ("<C-m> C-d"   . mc/remove-current-cursor)
+         ("<C-m> C-k"   . mc/remove-cursors-at-eol)
+         ("<C-m> M-d"   . mc/remove-duplicated-cursors)
+         ("<C-m> |"     . mc/move-to-column)
+         ("<C-m> ~"     . mc/compare-chars)))
+
+(use-package mc-freeze
+  :after multiple-cursors
+  :bind ("<C-m> f" . mc/freeze-fake-cursors-dwim))
+
+(use-package mc-rect
+  :after multiple-cursors
+  :bind ("<C-m> ]" . mc/rect-rectangle-to-multiple-cursors))
+
 (use-package midnight
   :bind ("C-c z" . clean-buffer-list))
 
@@ -315,10 +344,81 @@
   :bind
   ("C-c d" . multi-line))
 
+(use-package multiple-cursors
+  :ensure t
+  :after phi-search
+  :defer 5
+
+  ;; - Sometimes you end up with cursors outside of your view. You can scroll
+  ;;   the screen to center on each cursor with `C-v` and `M-v`.
+  ;;
+  ;; - If you get out of multiple-cursors-mode and yank - it will yank only
+  ;;   from the kill-ring of main cursor. To yank from the kill-rings of every
+  ;;   cursor use yank-rectangle, normally found at C-x r y.
+
+  :bind (("<C-m> ^"     . mc/edit-beginnings-of-lines)
+         ("<C-m> `"     . mc/edit-beginnings-of-lines)
+         ("<C-m> $"     . mc/edit-ends-of-lines)
+         ("<C-m> '"     . mc/edit-ends-of-lines)
+         ("<C-m> R"     . mc/reverse-regions)
+         ("<C-m> S"     . mc/sort-regions)
+         ("<C-m> W"     . mc/mark-all-words-like-this)
+         ("<C-m> Y"     . mc/mark-all-symbols-like-this)
+         ("<C-m> a"     . mc/mark-all-like-this-dwim)
+         ("<C-m> c"     . mc/mark-all-dwim)
+         ("<C-m> l"     . mc/insert-letters)
+         ("<C-m> n"     . mc/insert-numbers)
+         ("<C-m> r"     . mc/mark-all-in-region)
+         ("<C-m> s"     . set-rectangular-region-anchor)
+         ("<C-m> %"     . mc/mark-all-in-region-regexp)
+         ("<C-m> t"     . mc/mark-sgml-tag-pair)
+         ("<C-m> w"     . mc/mark-next-like-this-word)
+         ("<C-m> x"     . mc/mark-more-like-this-extended)
+         ("<C-m> y"     . mc/mark-next-like-this-symbol)
+         ("<C-m> C-x"   . reactivate-mark)
+         ("<C-m> C-SPC" . mc/mark-pop)
+         ("<C-m> ("     . mc/mark-all-symbols-like-this-in-defun)
+         ("<C-m> C-("   . mc/mark-all-words-like-this-in-defun)
+         ("<C-m> M-("   . mc/mark-all-like-this-in-defun)
+         ("<C-m> ["     . mc/vertical-align-with-space)
+         ("<C-m> {"     . mc/vertical-align)
+
+         ("S-<down-mouse-1>")
+         ("S-<mouse-1>" . mc/add-cursor-on-click))
+
+  :bind (:map selected-keymap
+              ("c"   . mc/edit-lines)
+              ("."   . mc/mark-next-like-this)
+              ("<"   . mc/unmark-next-like-this)
+              ("C->" . mc/skip-to-next-like-this)
+              (","   . mc/mark-previous-like-this)
+              (">"   . mc/unmark-previous-like-this)
+              ("C-<" . mc/skip-to-previous-like-this)
+              ("y"   . mc/mark-next-symbol-like-this)
+              ("Y"   . mc/mark-previous-symbol-like-this)
+              ("w"   . mc/mark-next-word-like-this)
+              ("W"   . mc/mark-previous-word-like-this))
+
+  :preface
+  (defun reactivate-mark ()
+    (interactive)
+    (activate-mark)))
+
 (use-package perspective
   :ensure t
   :config
   (persp-mode 1))
+
+(use-package phi-search
+  :ensure t
+  :defer 5)
+
+(use-package phi-search-mc
+  :ensure t
+  :after (phi-search multiple-cursors)
+  :config
+  (phi-search-mc/setup-keys)
+  (add-hook 'isearch-mode-mode #'phi-search-from-isearch-mc/setup-keys))
 
 (use-package powerline
   :ensure t)
@@ -335,6 +435,13 @@
   :demand t
   :config
   (save-place-mode 1))
+
+(use-package selected
+  :ensure t
+  :demand t
+  :delight selected-minor-mode
+  :config
+  (selected-global-mode 1))
 
 (use-package smart-mode-line
   :ensure t
@@ -369,8 +476,21 @@
   :ensure t
   :after (ivy)
 
+  :bind (:map swiper-map
+              ("M-y" . yank)
+              ("M-%" . swiper-query-replace)
+              ("C-." . swiper-avy)
+              ;; ("M-c" . swiper-mc)
+              ("M-c" . haba/swiper-mc-fixed)
+              )
   :bind
-  ("C-s" . swiper))
+  ("C-s" . swiper)
+
+  :config
+  (defun haba/swiper-mc-fixed ()
+    (interactive)
+    (setq swiper--current-window-start nil)
+    (swiper-mc)))
 
 (use-package thingatpt
   :demand t)
